5d277b79a693dc5132a6bbf1f303f92d4bebb5ac
eli miller <elijahmiller237@gmail.com>
2020-11-22 11:19:31 -0500

    fixed dropdown issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,63 +1,64 @@
 
 export default function Navbar() {
-    return (
-      <div>
-        <ul id="dropdown1" className="dropdown-content">
-          <li>
-            <a className="black-text" href="/logout">
-              Logout<i className="material-icons right">exit_to_app</i>
-            </a>
-          </li>
-        </ul>
-        <nav>
-          <div className="nav-wrapper grey lighten-5">
-            <a href="/" className="nav-logo"><img src="/logo.png" style={{ width: "60px"}} /></a>
-            <ul className="right hide-on-med-and-down">
-              <li>
-                <a className="black-text" href="/manager/shuttlesmanagment">
-                  Shuttles Managment
-                  <i className="small material-icons right">directions_bus</i>
-                </a>
-              </li>
-              <li>
-                <a className="black-text" href="/manager/reservations">
-                  Reservations<i className="material-icons right">event_available</i>
-                </a>
-              </li>
-              <li>
-                <a className="black-text" href="/manager/placemanagment">
-                  Places Managment<i className="material-icons right">place</i>
-                </a>
-              </li>
-              <li>
-                <a className="black-text" href="/manager/hoursupdate">
-                  Schedules<i className="material-icons right">access_time</i>
-                </a>
-              </li>
-              <li>
-                <a className="black-text" href="/manager/screens">
-                  Screens<i className="material-icons right">ad_units</i>
-                </a>
-              </li>
-              <li>
-                <a className="black-text" href="/manager/register">
-                  Register<i className="material-icons right">person_add_alt_1</i>
-                </a>
-              </li>
-              <li>
-                <a className="black-text" href="/manager/pushnotifications">
-                  Push notifications
-                  <i className="material-icons right">sms</i>
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-trigger" style={{ height: "100%" }} href="#!" data-target="#dropdown1" data-beloworigin="true" data-hover="true">
-                  <img className="circle circle-small" src="/logo.png"/>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    )
+  return (
+    <div>
+      <nav>
+        <div className="nav-wrapper grey lighten-5">
+          <a href="/" className="nav-logo"><img src="/logo.png" style={{ width: "60px"}} /></a>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <a className="black-text" href="/manager/shuttlesmanagment">
+                Shuttles Managment
+                <i className="small material-icons right">directions_bus</i>
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/manager/reservations">
+                Reservations<i className="material-icons right">event_available</i>
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/manager/placemanagment">
+                Places Managment<i className="material-icons right">place</i>
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/manager/hoursupdate">
+                Schedules<i className="material-icons right">access_time</i>
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/manager/screens">
+                Screens<i className="material-icons right">ad_units</i>
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/manager/register">
+                Register<i className="material-icons right">person_add_alt_1</i>
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/manager/pushnotifications">
+                Push notifications
+                <i className="material-icons right">sms</i>
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-trigger black-text" href="#" data-activates="dropdown1" data-beloworigin="true" style={{ textAlign: "center" }}>
+                Placeholder
+                <img className="circle circle-small" src="/placeholder-avatar.png"/>
+              </a>
+              <ul id="dropdown1" className="dropdown-content">
+                <li>
+                  <a className="black-text" href="/logout">
+                    Logout<i className="material-icons right">exit_to_app</i>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -12,14 +12,14 @@ class MainDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css" />
-          <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+          <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossOrigin="anonymous"></script>
           <script src="https://use.fontawesome.com/8ff98e4aea.js"></script>
         </Head>
         <body>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+          <script src="javascript.js"></script>
           <Main />
           <NextScript />
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-          <script src="/javascript.js"></script>
         </body>
       </Html>
     )
