ebc0ec47830a2b3d4387f115b301e29dc249958e
eli miller <elijahmiller237@gmail.com>
2020-11-22 12:51:36 -0500

    added links to navbar, added starter code for each page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,47 +1,67 @@
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="fixed-top">
       <nav>
         <div className="nav-wrapper grey lighten-5">
-          <a href="/" className="nav-logo"><img src="/logo.png" style={{ width: "60px"}} /></a>
+          <Link href="/"><a className="nav-logo"><img src="/logo.png" style={{ width: "60px"}} /></a></Link>
           <ul className="right hide-on-med-and-down">
             <li>
-              <a className="black-text" href="/manager/shuttlesmanagment">
-                Shuttles Managment
-                <i className="small material-icons right">directions_bus</i>
-              </a>
+              <Link href="/shuttles-management">
+                <a className="black-text">
+                  Shuttles Managment
+                  <i className="small material-icons right">directions_bus</i>
+                </a>
+              </Link>
             </li>
             <li>
-              <a className="black-text" href="/manager/reservations">
-                Reservations<i className="material-icons right">event_available</i>
-              </a>
+              <Link href="/reservations">
+                <a className="black-text">
+                  Reservations
+                  <i className="material-icons right">event_available</i>
+                </a>
+              </Link>
             </li>
             <li>
-              <a className="black-text" href="/manager/placemanagment">
-                Places Managment<i className="material-icons right">place</i>
-              </a>
+              <Link href="/places-management">
+                <a className="black-text">
+                  Places Managment
+                  <i className="material-icons right">place</i>
+                </a>
+              </Link>
             </li>
             <li>
-              <a className="black-text" href="/manager/hoursupdate">
-                Schedules<i className="material-icons right">access_time</i>
-              </a>
+              <Link href="/schedules">
+                <a className="black-text">
+                  Schedules
+                  <i className="material-icons right">access_time</i>
+                </a>
+              </Link>
             </li>
             <li>
-              <a className="black-text" href="/manager/screens">
-                Screens<i className="material-icons right">ad_units</i>
-              </a>
+              <Link href="/screens">
+                <a className="black-text">
+                  Screens
+                  <i className="material-icons right">ad_units</i>
+                </a>
+              </Link>
             </li>
             <li>
-              <a className="black-text" href="/manager/register">
-                Register<i className="material-icons right">person_add_alt_1</i>
-              </a>
+              <Link href="/register">
+                <a className="black-text">
+                  Register
+                  <i className="material-icons right">person_add_alt_1</i>
+                </a>
+              </Link>
             </li>
             <li>
-              <a className="black-text" href="/manager/pushnotifications">
-                Push notifications
-                <i className="material-icons right">sms</i>
-              </a>
+              <Link href="/push-notifications">
+                <a className="black-text">
+                  Push notifications
+                  <i className="material-icons right">sms</i>
+                </a>
+              </Link>
             </li>
             <li>
               <a className="dropdown-trigger black-text" href="#" data-activates="dropdown1" data-beloworigin="true" style={{ textAlign: "center" }}>
@@ -50,8 +70,10 @@ export default function Navbar() {
               </a>
               <ul id="dropdown1" className="dropdown-content">
                 <li>
+                  {/* logout not yet implented */}
                   <a className="black-text" href="/logout">
-                    Logout<i className="material-icons right">exit_to_app</i>
+                    Logout
+                    <i className="material-icons right">exit_to_app</i>
                   </a>
                 </li>
               </ul>
---
+++ pages/places-management.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function PlacesManagement() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Branda - Places Management</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
