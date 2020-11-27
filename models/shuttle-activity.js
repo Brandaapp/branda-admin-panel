38fc773e27a9e39729965ccfb87aa1a97c9666cf
Elijah Miller <elijahmiller@brandeis.edu>
2020-11-27 16:56:50 -0500

    added shuttle-activity schema

---
+++ models/shuttle-activity.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// This is the schema for the shuttle activity happening in one day at Brandeis
+
+const shuttleActivitySchema = new Schema({
+  date: Date,
+  times: [
+    {
+      start: Date,
+      end: Date,
+      busID: String,
+      busName: String,
+      route: String,
+      ID: String
+    }
+  ]
+});
+// THINK ABOUT CHANGING busID to samsaraID
+module.exports = mongoose.models.shuttleActivity || mongoose.model("shuttleActivity", shuttleActivitySchema);
