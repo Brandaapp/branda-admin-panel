a1031837dc2de5f19a7d95ba04fffea629b94c83
Elijah Miller <elijahmiller@brandeis.edu>
2020-11-30 12:37:02 -0500

    add patch method to /date endpoint

--- pages/api/shuttles/[date].js
+++ pages/api/shuttles/[date].js
@@ -6,21 +6,47 @@ dbConnect();
 
 export default (req, res) => {
     const { query: { date } } = req;
-    const day = DateTime.fromISO(date);
-    ShuttleActivity.findOne({ 
-        date: { 
-            $gte: day.startOf('day').toJSDate(), 
-            $lte: day.endOf('day').toJSDate()
-        } 
-    }, (err,doc) => {
-        if (err) {
-            console.log("Error finding date",err);
-            res.sendStatus(500);
-        } else if (!doc) {
-            console.log("Could not find date");
-            res.sendStatus(404);
-        } else {
-            res.send(doc);
+    const start = DateTime.fromISO(date).startOf('day').toJSDate();
+    const end = DateTime.fromISO(date).endOf('day').toJSDate();
+    if (req.method === 'PATCH') {
+        let temp = {
+            start: req.body.start,
+            end: req.body.end,
+            busID: req.body.busID,
+            busName: req.body.busName,
+            route: req.body.route,
+            ID: req.body.ID
         }
-    });
+        ShuttleActivity.update(
+            { date: { $gte: start, $lte: end }, "times.ID": { $ne: req.body.ID } },
+            { $addToSet: { times: temp } },
+            (err, doc) => {
+                if (err) {
+                    console.log("Error adding shuttle");
+                    res.sendStatus(500);
+                } else {
+                    res.send(doc);
+                    
+                    //TODO: connect to samsara api
+                }
+            }
+        )
+    } else {
+        ShuttleActivity.findOne({
+            date: {
+                $gte: start,
+                $lte: end
+            }
+        }, (err, doc) => {
+            if (err) {
+                console.log("Error finding date", err);
+                res.sendStatus(500);
+            } else if (!doc) {
+                console.log("Could not find date");
+                res.sendStatus(404);
+            } else {
+                res.send(doc);
+            }
+        });
+    }
 }
