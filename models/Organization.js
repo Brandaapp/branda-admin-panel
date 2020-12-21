406d2146e5c7af60775491d8581cf177898543d4
Sam Brenner <samuelbrenner@brandeis.edu>
2020-12-21 17:27:12 -0500

    init and adding Organization model

---
+++ models/Organization.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrganizationSchema = new Schema({
+  name: {
+    type: String
+  },
+  category: {
+    type: String,
+    default: "Club"
+  },
+  members: [String], //the push token of each member of this club
+  maxMessagesAllowed: {
+    type: Number,
+    default: 5
+  },
+  messageNumber: {
+    type: Number, //where this message falls in the chronological order of all messages sent by this org
+    default: 0
+  },
+  messagesSent: [
+    {
+      title: String,
+      message: String,
+      date: Date,
+      deliveredSuccessfully: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ],
+  active: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Organization = mongoose.model("Organization", OrganizationSchema);
+module.exports = Organization;
