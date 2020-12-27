9d37d7479be3545bc5c54823c814350b4fecb367
Yulu_Pan <panyulu2000@gmail.com>
2020-12-27 15:27:14 -0500

    adding css

--- components/ShuttleView.js
+++ components/ShuttleView.js
@@ -1,43 +1,59 @@
-import { useState, useEffect } from 'react';
+import shadows from "@material-ui/core/styles/shadows";
+import { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
 
-const axios = require('axios');
+const axios = require("axios");
 
 export default function ShuttleView() {
-    const [ date, setDate ] = useState(new Date("2019-09-16T22:40:27.884+00:00"));
-    const [ shuttles, setShuttles ] = useState(null);
+  const [date, setDate] = useState(new Date("2019-09-16T22:40:27.884+00:00"));
+  const [shuttles, setShuttles] = useState(null);
 
-    useEffect(async () => {
-        if (shuttles === null) {
-            await axios.get(`/api/shuttles/${date.toISOString()}`)
-            .then((response) => {
-                console.log(response.data);
-                setShuttles(response.data);
-            })
-            .catch(err => console.log("Error getting shuttles",err));
-        }
-    });
-
-    function renderShuttles() {
-        return shuttles.times.map(time => {
-            return (
-                <li>
-                    <div style={{ backgroundColor: "blue", border: "10px solid black" }}>
-                        <div style={{ color: "white" }}>Bus: {time.busName}</div><br />
-                        <div style={{ color: "white" }}>ID: {time.ID}</div><br />
-                        <div style={{ color: "white" }}>Route: {time.route}</div><br />
-                    </div>
-                </li>
-            );
+  useEffect(async () => {
+    if (shuttles === null) {
+      await axios
+        .get(`/api/shuttles/${date.toISOString()}`)
+        .then((response) => {
+          console.log(response.data);
+          setShuttles(response.data);
         })
+        .catch((err) => console.log("Error getting shuttles", err));
     }
+  });
 
-    if (shuttles !== null) {
-        return (
-            <div>
-                <ul>
-                    {renderShuttles()}
-                </ul>
+  function renderShuttles() {
+    return shuttles.times.map((time) => {
+      return (
+        <li>
+          <div className={styles.shuttleCard}>
+            <img src="" alt="Vehicle Icon" className={styles.shuttleImg}></img>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                marginRight: "5%",
+                flex: "1 0 auto",
+              }}
+            >
+              <div>
+                <h5>{time.busName}</h5>
+              </div>
+              <div className={styles.shuttleDetail}>
+                <sub>ID: {time.ID}</sub>
+                <br />
+                <sub>Route: {time.route}</sub>
+              </div>
             </div>
-        )
-    } else return null;
+          </div>
+        </li>
+      );
+    });
+  }
+
+  if (shuttles !== null) {
+    return (
+      <div>
+        <ul>{renderShuttles()}</ul>
+      </div>
+    );
+  } else return null;
 }
