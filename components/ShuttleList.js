36d80b0bd7978631561f29fe702a4fc3aa4df1de
Yulu_Pan <panyulu2000@gmail.com>
2021-01-10 16:15:46 -0500

    minor changes

--- components/ShuttleList.js
+++ components/ShuttleList.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Home.module.css";
-import { useState } from 'react';
+import { useState } from "react";
 import Remove from "@material-ui/icons/Remove";
 import Fab from "@material-ui/core/Fab";
 import ShuttleCard from "./ShuttleCard";
@@ -8,7 +8,7 @@ import ShuttleForm from "./ShuttleForm";
 const axios = require("axios");
 
 export default function ShuttleList(props) {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   if (props.shuttles !== null) {
     return (
@@ -19,18 +19,39 @@ export default function ShuttleList(props) {
               <h4 className="blue-text text-darken-1">{props.route}</h4>
             </div>
             <div className="center-align" style={{ paddingTop: "50px" }}>
-              <a className="btn-floating waves-effect waves-light blue" onClick={() => setShow(true)}>
+              <a
+                className="btn-floating waves-effect waves-light blue"
+                onClick={() => setShow(true)}
+              >
                 <i className="material-icons">add</i>
               </a>
             </div>
             <div style={{ display: "flex", flexDirection: "column" }}>
               {props.shuttles.map((time) => {
-                return <ShuttleCard time={time} route={props.route} getShuttles={props.getShuttles} date={props.date} key={"_" + Math.random().toString(36).substr(2, 9)} />;
+                return (
+                  <ShuttleCard
+                    time={time}
+                    route={props.route}
+                    getShuttles={props.getShuttles}
+                    date={props.date}
+                    key={"_" + Math.random().toString(36).substr(2, 9)}
+                  />
+                );
               })}
             </div>
           </div>
         </div>
-        {props.route === "Campus" || props.route === "campus" ? <ShuttleForm date={props.date} setShow={setShow} show={show} route={props.route} /> : null}
+        {
+          //props.route === "Campus" || props.route === "campus" ? (
+          <ShuttleForm
+            date={props.date}
+            setShow={setShow}
+            show={show}
+            route={props.route}
+            getShuttles={props.getShuttles}
+          />
+          //) : null
+        }
       </div>
     );
   } else return null;
