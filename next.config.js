0a245b2d94ee7ba35993aa69753f1817095ab186
Elijah Miller <elijahmiller237@gmail.com>
2021-09-03 14:38:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,11 +7,5 @@ module.exports = {
         })
     
         return config
-    },
-    env: {
-        MONGO_URI: "mongodb+srv://branda:branda*1234@kevin.0cusy.mongodb.net/brandadb?retryWrites=true&w=majority",
-        PORT: 3000
     }
-};
-
-//mongoshell connection string: "mongodb+srv://kevin.0cusy.mongodb.net/brandadb" --username branda
+    
