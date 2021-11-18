6c79d5fe79e79db6f33648e323042e8558f7b67c
Sam Brenner <samuelbrenner@brandeis.edu>
2021-11-18 15:48:57 -0500

    fixing dbconnect import in notifications index

--- pages/api/getinfo/branda/notifications/index.js
+++ pages/api/getinfo/branda/notifications/index.js
@@ -1,34 +1,33 @@
-import dbConnect from "../../../../utils/dbConnect";
+import dbConnect from "../../../../../utils/dbConnect";
 import BranvanNotif from "../../../../../models/Announcement";
 
 dbConnect();
 
-export default function (req, res) {
-  BranvanNotif.find(
-    {
-      startTime: { $lte: moment().toDate() },
-      endTime: { $gte: moment().toDate() },
-    },
-    (err, docs) => {
-      if (err) {
-        res.send({ empty: true });
-      } else {
-        if (docs.length === 0) {
-          res.send({ empty: true });
-        } else {
-          res.send({
-            empty: false,
-            notifications: docs.map((doc) => {
-              return {
-                type: doc.type,
-                content: doc.content,
-                startTime: doc.startTime,
-                endTime: doc.endTime,
-              };
-            }),
-          });
+export default function(req, res) {
+    BranvanNotif.find({
+            startTime: { $lte: moment().toDate() },
+            endTime: { $gte: moment().toDate() },
+        },
+        (err, docs) => {
+            if (err) {
+                res.send({ empty: true });
+            } else {
+                if (docs.length === 0) {
+                    res.send({ empty: true });
+                } else {
+                    res.send({
+                        empty: false,
+                        notifications: docs.map((doc) => {
+                            return {
+                                type: doc.type,
+                                content: doc.content,
+                                startTime: doc.startTime,
+                                endTime: doc.endTime,
+                            };
+                        }),
+                    });
+                }
+            }
         }
-      }
-    }
-  );
+    );
 }
