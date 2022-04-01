1d7fbfdb33cd6037905784368a9e8a58f10e058f
Sam Brenner <samuelbrenner@brandeis.edu>
2022-04-01 18:58:02 -0400

    missing news import

--- pages/api/getinfo/news/index.js
+++ pages/api/getinfo/news/index.js
@@ -1,13 +1,14 @@
 import dbConnect from "../../../../utils/dbConnect";
+import News from "../../../../models/News";
 
 dbConnect();
 
-export default function (_req, res) {
-  News.find().exec((err, articles) => {
-    if (err) {
-      res.send({ err: err });
-    } else {
-      res.send({ articles: articles });
-    }
-  });
+export default function(_req, res) {
+    News.find().exec((err, articles) => {
+        if (err) {
+            res.send({ err: err });
+        } else {
+            res.send({ articles: articles });
+        }
+    });
 }
