5c170945a31e7eab44c390cf15675270d45abe49
Sam Brenner <samuelbrenner@brandeis.edu>
2022-09-16 04:14:08 -0400

    splitting frontend-used pushtoken routes

--- pages/api/getinfo/pushtokens/index.js
+++ pages/api/getinfo/pushtokens/index.js
@@ -4,24 +4,33 @@ import Organization from "../../../../models/Organization";
 dbConnect();
 
 export default (req, res) => {
-  Organization.findOneAndUpdate(
-    {
-      name: req.body.data.organization_name,
-    },
-    {
-      $addToSet: {
-        members: req.body.data.pushToken,
-      },
-    },
-    { safe: true, upsert: true },
-    (err, doc) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json({
-          organization: doc,
-        });
-      }
+  return new Promise(resolve => {
+    if (req.method === 'POST') {
+      const { data: { organization_name, pushToken } } = 
+        typeof req.body === "object" ? req.body : JSON.parse(req.body);
+      Organization.findOneAndUpdate(
+        {
+          name: organization_name,
+        },
+        {
+          $addToSet: {
+            members: pushToken,
+          },
+        },
+        { safe: true, upsert: true, useFindAndModify: false },
+        (err, doc) => {
+          if (err) {
+            res.status(500).send({ err });
+            resolve();
+          } else {
+            res.json({ organization: doc });
+            resolve();
+          }
+        }
+      );
+    } else {
+      res.status(405).send(`HTTP method must be POST on ${req.url}`);
+      resolve();
     }
-  );
+  });
 };
