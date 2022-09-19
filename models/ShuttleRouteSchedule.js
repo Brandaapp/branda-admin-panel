9e3b6f2617fba749c6a040e66c8e2a247e9cdcf9
sabrenner <samuelbrenner@brandeis.edu>
2022-09-19 13:15:01 -0400

    adding route

---
+++ models/ShuttleRouteSchedule.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ShuttleRouteScheduleSchema = new Schema({
+    name: String,
+    data: Object
+});
+
+module.exports = mongoose.models.ShuttleRouteSchedule || mongoose.model('ShuttleRouteSchedule', ShuttleRouteScheduleSchema);
