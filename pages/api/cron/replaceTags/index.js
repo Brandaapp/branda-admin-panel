30015ddee7941448e0fc9cbccc6b01de75192180
samuelbrenner <54922906+sbrenner132@users.noreply.github.com>
2022-09-18 23:55:30 -0400

    Fix Cron Jobs (#48)
    * adding dining hours cron and actions for cron jobs
    
    * change actions yml
    
    * fixing cron typo
    
    * adding resolves where forgotten

--- pages/api/cron/replaceTags/index.js
+++ pages/api/cron/replaceTags/index.js
@@ -5,63 +5,64 @@ import shuttleActivity from "../../../../models/ShuttleActivity";
 
 dbConnect();
 
-export default async function(_req, res) {
-    // res.send("Hello world");
-    console.log("EXECUTING REPLACE TAGS")
-    let routesToUpdate = ["Campus", "Waltham", "Boston"]
-    let tags = await fetch(`https://api.samsara.com/v1/tags?access_token=${process.env.SAMSARA_API_KEY}`)
-        .then(response => response.json())
-        .then((response) => {
-            console.log(response);
-            return response
-        })
-        .then(json => json.tags.map(tag => {
-            return {
-                id: tag.id,
-                name: tag.name,
-                groupId: tag.groupId
-            }
-        }))
-        .then(tags => tags.filter(tag => routesToUpdate.includes(tag.name)))
-    console.log("TAGS", tags)
+export default function(req, res) {
+    return new Promise(async (resolve) => {
+        if (req.method === 'PATCH') {
+            const routesToUpdate = ["Campus", "Waltham", "Boston"];
+            const tags = await fetch(`https://api.samsara.com/v1/tags?access_token=${process.env.SAMSARA_API_KEY}`)
+                .then(response => response.json())
+                .then(json => json.tags.map(tag => {
+                    return {
+                        id: tag.id,
+                        name: tag.name,
+                        groupId: tag.groupId
+                    }
+                }))
+                .then(tags => tags.filter(tag => routesToUpdate.includes(tag.name)));
 
-    let currentState = await shuttleActivity.findOne({
-        date: {
-            $gte: moment().startOf('day'),
-            $lte: moment().endOf('day')
-        }
-    }).exec()
-
-    // We're adding 1 minute here because there is otherwise an off-by-1 error
-    if (!!currentState) {
-        let now = moment().add(1, "minute")
-
-        let currentTags = currentState.times.filter(time => now >= time.start && now <= time.end)
-        console.log("CURRENT TAGS", currentTags)
-        const x = await Promise.all(tags.map(tag => {
-            console.log(currentTags.filter(item => item.route === tag.name).map(item => {
-                return {
-                    id: item.busID
+            const currentState = await shuttleActivity.findOne({
+                date: {
+                    $gte: moment().startOf('day'),
+                    $lte: moment().endOf('day')
                 }
-            }))
-            return fetch(`https://api.samsara.com/v1/tags/${tag.id}?access_token=${process.env.SAMSARA_API_KEY}`, {
-                method: "PUT",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: tag.name,
-                    vehicles: currentTags.filter(item => item.route === tag.name).map(item => {
-                        return {
-                            id: item.busID
-                        }
-                    })
-                })
-            }).then(response => response.text())
-        })).catch(e => res.status(500))
-        console.log("RESULT", x)
-        res.status(200);
-    } else {
-        res.status(200).send("No shuttle activity found");
-    }
+            }).exec();
+
+            // We're adding 1 minute here because there is otherwise an off-by-1 error
+            if (!!currentState) {
+                const now = moment().add(1, "minute");
+
+                const currentTags = currentState.times.filter(time => now >= time.start && now <= time.end);
+                
+                const x = await Promise.all(tags.map(async tag => {
+                    const response = await fetch(`https://api.samsara.com/v1/tags/${tag.id}?access_token=${process.env.SAMSARA_API_KEY}`, {
+                        method: "PUT",
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            name: tag.name,
+                            vehicles: currentTags.filter(item => item.route === tag.name).map(item_1 => {
+                                return {
+                                    id: item_1.busID
+                                };
+                            })
+                        })
+                    });
+                    return await response.text();
+                })).catch(err => {
+                    res.status(500).send({ err })
+                    resolve();
+                });
+                
+                res.status(200).send({});
+                resolve();
+            } else {
+                res.status(200).send("No shuttle activity found");
+                resolve();
+            }
+        } else {
+            res.status(405).send(`HTTP method must be PATCH on ${req.url}`);
+            resolve();
+        }
+    })
 };
