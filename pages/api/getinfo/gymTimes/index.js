4e9d196f8986c0292bbf0302f3a2f8f667cbfc4e
ximeng li <ximengli@brandeis.edu>
2022-10-10 12:21:29 -0400

    update getinfo/gymTimes

--- pages/api/getinfo/gymTimes/index.js
+++ pages/api/getinfo/gymTimes/index.js
@@ -23,7 +23,6 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 
 const getInfo = (doc) => {
   const info = []; // array that will eventually hold all of our JSON data
-  const fall2021Excls = moment('2021-08-25');
   // loop through each day of the week finding when each space is open on that day
   for (let i = 0; i < days.length; i++) {
     // each day has the spaces which themselves have the times at which they are open that day
@@ -31,30 +30,13 @@ const getInfo = (doc) => {
     weekDay.date = moment().day(i);
     spaceIds.forEach(space => { // loop through each space and get the info for each
       const activity = { name: space.name }; // initialize the space
-      const apiActivity = doc.data.filter(data => space.ids.includes(data.id));
-      apiActivity.forEach(obj => {
-        const day = obj.actual_open_hours[i];
-        const start = moment().startOf('day').add(day.hours[0].start, 'm');
-        const end = moment().startOf('day').add(day.hours[0].end, 'm');
-        if (moment().isSameOrBefore(fall2021Excls)) {
-          end.hour(20);
-          end.minute(0);
-        }
-        activity['time' + day.day] = start.format('HH:mm:ss') + '-' + end.format('HH:mm:ss');
-      });
-      /* for (let j = 0; j < space.ids.length; j++) { //for each of the ids associated with our space...
-          let id = space.ids[j];
-          for (let k = 0; k < doc.length; k++) { // ...look through each space in the doc to see if its id matches
-              let area = doc[k];
-              if (area.id === id) {
-                //get this spaces hours for the current day of the week
-                  let time = area.online_reservation_definition[i].summary[0];
-                  let hours = time.start + "-" + time.end;
-                  activity["time" + (j + 1)] = hours;
-                  k = doc.length; //exit the loop
-              }
-          }
-      } */
+      // there is no need to use a forEach loop to rewrite variables with the same value,
+      // since start and end times are the same for each activity, just take the first one
+      const apiActivity = doc.data.filter(data => space.ids.includes(data.id))[0];
+      const day = apiActivity.actual_open_hours[i];
+      const start = moment().startOf('day').add(day.hours[0].start, 'm');
+      const end = moment().startOf('day').add(day.hours[0].end, 'm');
+      activity['time' + day.day] = start.format('HH:mm:ss') + '-' + end.format('HH:mm:ss');
       weekDay.data.push(activity); // add the activity info to the array in our day object
     });
     info.push(weekDay); // add the day to our big array
