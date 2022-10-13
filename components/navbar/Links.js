0520a98df616f779b1f9c88e8012f7b6775e05a6
samuelbrenner <54922906+sbrenner132@users.noreply.github.com>
2022-10-13 09:42:07 -0400

    NavBar UI Re-Work (#69)

--- components/navbar/Links.js
+++ components/navbar/Links.js
@@ -1,29 +1,48 @@
-import { access } from '../../utils/rolesUtils.mjs';
+import { Button, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { useRouter } from 'next/router';
+import { access } from '../../utils/rolesUtils';
 import linksData from './linksData.json';
-import Link from 'next/link.js';
+import styled from 'styled-components';
 
-function renderLinks (type) {
-  const { allowed } = access[type];
-  return [...allowed]
-    .filter(link => Object.keys(linksData).includes(link)) // filter links for nav bar, not all allowed
-    .map((link, index) => {
-      return (
-        <li key={index}>
-          <Link href={link}>
-            <a className="black-text">
+const UnderlineButton = styled(Button)({
+  '&:hover': {
+    textDecoration: 'underline',
+    textDecorationColor: 'white',
+    textUnderlineOffset: '1em',
+    backgroundColor: 'transparent'
+  }
+});
+
+export default function Links ({ type, drawer }) {
+  const router = useRouter();
+
+  const renderLinks = (type, drawer) => {
+    const { allowed } = access[type];
+    return [...allowed]
+      .filter(link => Object.keys(linksData).includes(link))
+      .map((link, index) => {
+        return (
+          drawer
+            ? <ListItem key={`${index}`} disablePadding>
+              <ListItemButton onClick={() => { router.push(link); }}>
+                <ListItemText primary={linksData[link].name} />
+                <i className="material-icons right">{linksData[link].icon}</i>
+              </ListItemButton>
+            </ListItem>
+            : <UnderlineButton
+              key={`${index}`}
+              onClick={() => { router.push(link); }}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
               {linksData[link].name}
-              <i className="material-icons right">{linksData[link].icon}</i>
-            </a>
-          </Link>
-        </li>
-      );
-    });
-}
+            </UnderlineButton>
+        );
+      });
+  };
 
-export default function Links ({ type }) {
   return (
     <>
-      { renderLinks(type) }
+      { renderLinks(type, drawer) }
     </>
   );
 }
