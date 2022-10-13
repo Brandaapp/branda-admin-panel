b6351d0a3a792654c2f4510a12e4273956d2d0ae
sabrenner <samuelbrenner@brandeis.edu>
2022-10-12 23:37:57 -0400

    remove underline transform animation and icons

--- components/navbar/Links.js
+++ components/navbar/Links.js
@@ -4,30 +4,14 @@ import { access } from '../../utils/rolesUtils';
 import linksData from './linksData.json';
 import styled from 'styled-components';
 
-const UnderlineButton = styled(Button)`
-color: inherit;
-  margin-right: 1em;
-  text-decoration: none;
-  display: block;
-  position: relative;
-  overflow: hidden;
-  color: white;
-  border-radius: 5px 5px 0 0;
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 0.1em;
-    background-color: white;
-    opacity: 1;
-    transform: translate3d(-100%, 0, 0);
-    transition: opacity 300ms, transform 300ms;
+const UnderlineButton = styled(Button)({
+  '&:hover': {
+    textDecoration: 'underline',
+    textDecorationColor: 'white',
+    textUnderlineOffset: '1em',
+    backgroundColor: 'transparent'
   }
-  &:hover::after{
-    transform: translate3d(0, 0, 0);
-  }`;
+});
 
 export default function Links ({ type, drawer }) {
   const router = useRouter();
@@ -51,7 +35,6 @@ export default function Links ({ type, drawer }) {
               sx={{ my: 2, color: 'white', display: 'block' }}
             >
               {linksData[link].name}
-              <i className="material-icons right">{linksData[link].icon}</i>
             </UnderlineButton>
         );
       });
