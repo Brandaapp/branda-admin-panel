5598b60e8a6c3ed11ec0aded0fd0e8f7f79f68ab
Archer <heffernarcher@gmail.com>
2022-11-12 14:43:01 -0500

    added show/hide password button to loginForm

--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -17,9 +17,10 @@ import { getSession, signIn } from 'next-auth/client';
 import { access } from '../../utils/rolesUtils.mjs';
 import Link from 'next/link.js';
 
-export default function LoginForm () {
+export default function LoginForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPW, setShowPW] = useState(false);
 
   const router = useRouter();
 
@@ -48,7 +49,7 @@ export default function LoginForm () {
 
   return (
     <Container maxWidth='xs'>
-      { error
+      {error
         ? <Alert
           variant='standard'
           severity="error"
@@ -59,11 +60,11 @@ export default function LoginForm () {
         >
           {'Incorrect credentials provided'}
         </Alert>
-        : null }
+        : null}
       <Paper elevation={10} sx={{ py: 3, flexGrow: 1 }}>
         <Grid container display='flex' direction='column' alignItems='center'>
           <Grid item>
-            <Image alt='branda logo' src='/branda-logo-long-2.png' width={150} height={60}/>
+            <Image alt='branda logo' src='/branda-logo-long-2.png' width={150} height={60} />
           </Grid>
           <Divider sx={{ width: '90%', mt: 1 }} />
           <Grid item p={4} textAlign='center'>
@@ -92,12 +93,13 @@ export default function LoginForm () {
                 id='password'
                 label='Password'
                 variant='standard'
-                type='password'
+                type={showPW ? 'text' : 'password'}
                 sx={{ my: 1 }}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 onKeyDown={handleKeyPress}
               />
+              <Button onClick={() => setShowPW(!showPW)}>{showPW ? 'hide password' : 'show password'}</Button>
               <Button
                 sx={{ mt: 5 }}
                 variant='contained'
