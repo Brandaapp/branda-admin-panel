a61e83d868abf0d1c2b9841e1a9f9a20a8189378
Archer <heffernarcher@gmail.com>
2023-01-05 18:59:16 -0500

    connected to docker container for testing

--- zlocalTesting/localtest.sh
+++ zlocalTesting/localtest.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #///////////////////////////////////////////////////////////////////////////////////////////////////////#
 #
 # creates docker container of mongodb database and runs tests using containerized mongo instance
@@ -16,29 +17,35 @@
 # Windows: ? (google it)
 #
 # Linux: sudo apt install docker
+# snap install docker 
+# apt install docker.io
 #
 #///////////////////////////////////////////////////////////////////////////////////////////////////////#
 
 name=mongo_test_server
 
+# checks if docker is installed
+(sudo docker --version > /dev/null && \
+
 # pulls mongo image from docker
-sudo docker pull mongo | \
+sudo docker pull mongo > /dev/null 2>&1 ; \
 
 # deletes any previous containers with the same name
-sudo docker rm -f "$name" | \
+sudo docker rm -f "$name" > /dev/null 2>&1 ; \
 # starts container, or if does not exist, will create it and run it
 # ignore the error messages: Should be connected at part 8080
-sudo docker start "$name" || sudo docker run -p 8080:27017 --name "$name" -d mongo && \
+sudo docker run -p 8080:27017 --name "$name" -d mongo > /dev/null && \
 
 # enter the container ISSUE: script ends once entering the 
 # gain access to the mongo shell
-echo "Connected at http://localhost:8080"
-sleep 10
+echo "Connected at http://localhost:8080" && \
+echo "Running tests:\n" && \
 
-#TODO: make mongo avaliable outside the container
-    
 # TODO: run tests
 
-# end container process
-sudo docker stop "$name"
-sudo docker rm -f "$name"
+node ./zlocalTesting/runTests.mjs && \
+    
+echo "\nTests complete" && \
+# end container process and delete container
+sudo docker stop "$name" > /dev/null && \
+sudo docker rm -f "$name" > /dev/null) || "An issue has happened, please check that you have docker installed"
