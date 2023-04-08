22b93c2a8e2a373e2aa70f7f79c43c977c51630a
ArcherHeffern <heffernarcher@gmail.com>
2023-04-08 13:35:54 -0400

    please??

--- tests/utils/apiUtils.spec.mjs
+++ tests/utils/apiUtils.spec.mjs
@@ -1,16 +1,42 @@
 /* eslint-disable no-unused-expressions */
 import { expect } from 'chai';
-import mongoose from 'mongoose';
-const kittySchema = new mongoose.Schema({
-  name: String
-});
-const Kitten = mongoose.model('Kitten', kittySchema);
 
-describe('Archer tests', () => {
-  it('Correctly gets data from test database', async () => {
-    const cat = new Kitten({ name: 'Cat!' });
-    await cat.save();
-    const kittens = await Kitten.find();
-    expect(!!kittens).to.be.true;
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'testdb';
+
+describe('API tests', () => {
+  let client;
+  let db;
+
+  beforeEach(async () => {
+    // Connect to MongoDB
+    client = MongoClient.connect(url, { useUnifiedTopology: true });
+    db = client.db(dbName);
+
+    // Set up test data
+    await db.collection('myCollection').insertOne({ name: 'John Doe' });
+  });
+
+  afterEach(async () => {
+    // Remove test data
+    await db.collection('myCollection').deleteMany({});
+
+    // Disconnect from MongoDB
+    await client.close();
+  });
+
+  it('should retrieve data from the database', async () => {
+    const result = await db.collection('myCollection').findOne({ name: 'John Doe' });
+    expect(result.name).to.equal('John Doe');
+  });
+
+  it('should insert data into the database', async () => {
+    const data = { name: 'Jane Doe' };
+    await db.collection('myCollection').insertOne(data);
+    const result = await db.collection('myCollection').findOne(data);
+    expect(result).to.deep.equal(data);
   });
 });
