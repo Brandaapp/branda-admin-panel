9d20d76207cf74df5cfad30a7147caaeeba62b3c
ArcherHeffern <heffernarcher@gmail.com>
2023-04-08 12:35:36 -0400

    deleted ztest folder

--- tests/utils/tests.spec.mjs
+++ tests/utils/tests.spec.mjs
@@ -4,10 +4,10 @@ import mongoose from 'mongoose';
 const kittySchema = new mongoose.Schema({
   name: String
 });
+const Kitten = mongoose.model('Kitten', kittySchema);
 
 describe('Archer tests', () => {
   it('Correctly gets data from test database', async () => {
-    const Kitten = mongoose.model('Kitten', kittySchema);
     const cat = new Kitten({ name: 'Cat!' });
     await cat.save();
     const kittens = await Kitten.find();
