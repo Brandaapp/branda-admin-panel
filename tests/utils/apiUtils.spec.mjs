33b15944fc951b244a4c309fa62cdc4bcee460f0
ArcherHeffern <heffernarcher@gmail.com>
2023-04-12 21:10:53 -0400

    why dont we use typescript aghhhh

--- tests/utils/apiUtils.spec.mjs
+++ tests/utils/apiUtils.spec.mjs
@@ -47,7 +47,7 @@ describe('API tests', () => {
     expect(res.status).to.equal(200);
     expect((await res.json()).username).to.equal('Archer');
     const result = await db.collection('User').findOne(data);
-    expect(result.name).to.equal(data.name);
+    expect(result.username).to.equal(data.username);
   });
 
   it('should get all users', async () => {
